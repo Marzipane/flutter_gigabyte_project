0408f67c7ec2d28317839ac3fde893a0e6a32f1e
Marzipane <danil.ma2018@gmail.com>
2022-08-25 11:13:36 +0300

    UPD.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import '../widgets/header_bar.dart';
 import '../generated/l10n.dart';
+import '../widgets/image_carousel.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -16,6 +17,7 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       body: SingleChildScrollView(
         child: Column(children: [
+          FullscreenSlider(),
           GridView.count(
             // Create a grid with 2 columns. If you change the scrollDirection to
             // horizontal, this produces 2 rows.
